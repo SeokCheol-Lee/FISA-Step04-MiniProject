64843cda82b65695dd118971654b1d7559a81edc
seungunleeee <90971532+seungunleeee@users.noreply.github.com>
2024-07-12 16:17:15 +0900

    Revert "Dev/5"

--- src/model/entity/Comment.java
+++ src/model/entity/Comment.java
@@ -12,10 +12,6 @@ import lombok.ToString;
 @Builder
 @ToString
 public class Comment {
-<<<<<<< HEAD
-
-=======
->>>>>>> 36ae72f85d95d750fad770d6dd79e70dbbf6bf47
 	private int userId;
 	private int boardId;
 	private String context;
--- src/model/entity/User.java
+++ src/model/entity/User.java
@@ -2,7 +2,6 @@ package model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
 import model.GradeType;
@@ -12,10 +11,6 @@ import model.GradeType;
 @AllArgsConstructor
 @ToString
 public class User {
-<<<<<<< HEAD
-
-=======
->>>>>>> 36ae72f85d95d750fad770d6dd79e70dbbf6bf47
     private String name;
     private String email;
     private GradeType grade;
